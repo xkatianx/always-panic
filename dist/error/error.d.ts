4fcd1d2092e3b66859203d2730de9334b09dec82
xkatianx <23047821+xkatianx@users.noreply.github.com>
2026-05-26 15:58:31 +0800

    feat(error): add unreachable error handling and toOthers conversion (#5)
    * feat(error): add unreachable error handling and toOthers conversion
    
    Implemented a new unreachable error type in MyError with corresponding tests. The MyError class now includes a static method for creating unreachable errors and a method to convert existing errors to a MyError with the OTHERS code, preserving the original message and cause. Enhanced test coverage for these new functionalities.
    
    * build files
    
    * chore(package): bump version to 0.3.0

--- dist/error/error.d.ts
+++ dist/error/error.d.ts
@@ -2,12 +2,15 @@ import { MyErrorBase } from './base.js';
 export declare enum MyErrorCode {
     /** some errors thrown by others' packages not dealt */
     OTHERS = 0,
-    /** not used for now */
-    placeholder = 1
+    /** unreachable */
+    UNREACHABLE = 1
 }
 export declare class MyError<C extends MyErrorCode> extends MyErrorBase<C> {
     protected causeForUnwrap: boolean;
     constructor(code: C, message: string);
     static fromAny(e: unknown): MyError<MyErrorCode.OTHERS>;
+    static unreachable(reason?: string): MyError<MyErrorCode.UNREACHABLE>;
+    /** Convert to MyErrorCode.OTHERS */
+    toOthers(): MyError<MyErrorCode.OTHERS>;
 }
 //# sourceMappingURL=error.d.ts.map
--- src/error/error.ts
+++ src/error/error.ts
@@ -3,8 +3,8 @@ import { MyErrorBase } from './base.js'
 export enum MyErrorCode {
   /** some errors thrown by others' packages not dealt */
   OTHERS,
-  /** not used for now */
-  placeholder,
+  /** unreachable */
+  UNREACHABLE,
 }
 
 export class MyError<C extends MyErrorCode> extends MyErrorBase<C> {
@@ -21,6 +21,19 @@ export class MyError<C extends MyErrorCode> extends MyErrorBase<C> {
       Error.isError(e) ? e.message : String(e),
     )
     err.cause = e
+    return err
+  }
+
+  static unreachable(reason: string = '') {
+    return new MyError(MyErrorCode.UNREACHABLE, `unreachable: ${reason}`)
+  }
+
+  /** Convert to MyErrorCode.OTHERS */
+  toOthers() {
+    if (this.code === MyErrorCode.OTHERS) return this
+    const err = new MyError(MyErrorCode.OTHERS, this.message)
+    err.cause = this.cause
+    if (this.stack != null) err.stack = this.stack
     return err
   }
 }
