9fb18245fee063fe36eec3c371ab6e99ad5be1e8
katian <23047821+xkatianx@users.noreply.github.com>
2026-05-26 15:54:30 +0800

    build files

--- dist/error/error.js
+++ dist/error/error.js
@@ -3,8 +3,8 @@ export var MyErrorCode;
 (function (MyErrorCode) {
     /** some errors thrown by others' packages not dealt */
     MyErrorCode[MyErrorCode["OTHERS"] = 0] = "OTHERS";
-    /** not used for now */
-    MyErrorCode[MyErrorCode["placeholder"] = 1] = "placeholder";
+    /** unreachable */
+    MyErrorCode[MyErrorCode["UNREACHABLE"] = 1] = "UNREACHABLE";
 })(MyErrorCode || (MyErrorCode = {}));
 export class MyError extends MyErrorBase {
     causeForUnwrap = true;
@@ -15,6 +15,19 @@ export class MyError extends MyErrorBase {
     static fromAny(e) {
         const err = new MyError(MyErrorCode.OTHERS, Error.isError(e) ? e.message : String(e));
         err.cause = e;
+        return err;
+    }
+    static unreachable(reason = '') {
+        return new MyError(MyErrorCode.UNREACHABLE, `unreachable: ${reason}`);
+    }
+    /** Convert to MyErrorCode.OTHERS */
+    toOthers() {
+        if (this.code === MyErrorCode.OTHERS)
+            return this;
+        const err = new MyError(MyErrorCode.OTHERS, this.message);
+        err.cause = this.cause;
+        if (this.stack != null)
+            err.stack = this.stack;
         return err;
     }
 }
